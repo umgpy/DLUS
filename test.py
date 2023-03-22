4a71f5ff9524115d2f51d99b4719cabb80385309
luciacubero <83298381+luciacubero@users.noreply.github.com>
2023-03-22 16:36:11 +0100

    Update test.py

--- test.py
+++ test.py
@@ -32,7 +32,9 @@ print("-------------------------------------------------------------------------
 # CT and MR data have to be loaded separately, to indicate if they are in DICOM or NIfTI format
 load_data(data_path, out_path, mode, modality=modality)
 
-# 2. VOI Extraction: Localization Network + Crop using the centroid of the coarse segmentation
+# 2. VOI Extraction: Localization Network + Crop using the centroid of the coarse segmentation. Check the result to ensure that
+# appropriate VOI has been created. Sometimes some images are not well predicted and it's necessary to modify this VOI manually
+# to ensure that the OARs and urethra segmentations are accurate.
 print("-------------------------------------------------------------------------------------------------------")
 print("2. VOI EXTRACTION...")
 print("-------------------------------------------------------------------------------------------------------")
@@ -49,38 +51,42 @@ print("-------------------------------------------------------------------------
 print("3. OARs SEGMENTATION (OUTPUT 1)...")
 print("-------------------------------------------------------------------------------------------------------")
 # Environmnet Variables
-data_path = os.getcwd()+'/SegmentationNet'
-main_dir  = os.path.join(data_path,'nnUNet')
+main_dir  = os.path.join(os.getcwd(), 'networks/SegmentationNet/nnUNet')
 # Can be set in the the .bashrc file or exported in the terminal:
 os.environ['nnUNet_raw_data_base'] = os.path.join(main_dir,'nnUNet_raw_data_base')
 os.environ['nnUNet_preprocessed']  = os.path.join(main_dir,'nnUNet_preprocessed')
 os.environ['RESULTS_FOLDER']       = os.path.join(main_dir,'nnUNet_trained_models')
 os.environ['MKL_SERVICE_FORCE_INTEL'] = "1"
 
-path_imgs1    = out_path+'/VOIs/imagesTs'
-path_results1 = out_path+'/OARs/'+model
+path_imgs    = os.path.join(out_path, 'VOIs', 'imagesTs')
+dir_ddbb_OARs = os.path.join(out_path, 'OARs', model)
 
+# Predict OAR segmentations (rectum, bladder, prostate, seminal vesicles)
 if model=='FR_model':
-    !nnUNet_predict -i {path_imgs1} -o {path_results1} -t 112 -tr nnUNetTrainerV2 -ctr nnUNetTrainerV2CascadeFullRes -m 3d_fullres -p nnUNetPlansv2.1 --disable_tta
+    !nnUNet_predict -i {path_imgs} -o {dir_ddbb_OARs} -t 112 -tr nnUNetTrainerV2 -ctr nnUNetTrainerV2CascadeFullRes -m 3d_fullres -p nnUNetPlansv2.1 --disable_tta
     
 if model=='Mixed_model':
-    !nnUNet_predict -i {path_imgs1} -o {path_results1} -t 113 -tr nnUNetTrainerV2 -ctr nnUNetTrainerV2CascadeFullRes -m 3d_fullres -p nnUNetPlansv2.1 --disable_tta
+    !nnUNet_predict -i {path_imgs} -o {dir_ddbb_OARs} -t 113 -tr nnUNetTrainerV2 -ctr nnUNetTrainerV2CascadeFullRes -m 3d_fullres -p nnUNetPlansv2.1 --disable_tta
     
-dir_ddbb_OARs = out_path+'/OARs/'+model
-for i, file_out_OARs in enumerate(sorted(glob(dir_ddbb_OARs + r'/*.nii.gz'))):
-    idx = file_out_OARs.split('/')[-1].split('_')[-1][:-7]
-    file_OARs = dir_ddbb_OARs+'/IGRT_'+idx+'.nii.gz'
+for i, file_OARs in enumerate(sorted(glob(dir_ddbb_OARs + r'/*.nii.gz'))):
+    idx = file_OARs.split('/')[-1].split('_')[-1].split('.')[0]
     print(file_OARs)
     out_OARs  = sitk.ReadImage(file_OARs)
+    out_VOI   = sitk.ReadImage(os.path.join(path_imgs, ddbb+'_'+idx+'_0000.nii.gz'))
+    
     # 3.1 Post-processing to Native space
     print("3.1 OARs SEGMENTATION POST-PROCESSING TO NATIVE SPACE -------------------------------------------------")
-    postprocessing_native(out_OARs, out_path, str(idx),'OARs', None, False)
-    postprocessing_native(out_OARs, out_path, str(idx),'voi', path_imgs1, False)
+    postprocessing_native(out_OARs, out_path, str(idx),'OARs', ddbb, modality)
+    postprocessing_native(out_VOI, out_path, str(idx),'VOI', ddbb, modality)
+    
     # 3.2 Exportation to Dicom-RT
     print("3.2 OARs SEGMENTATION EXPORTATION TO DICOM-RT ---------------------------------------------------------")
-    ct_file  = out_path+'/Native/ID_'+str(idx)+'/ID_'+str(idx)+'_voi.nii.gz'
-    seg_file = out_path+'/Native/ID_'+str(idx)+'/ID_'+str(idx)+'_OARs.nii.gz'
-    export2dicomRT(ct_file, seg_file, out_path+'/Native/DICOM/ID_'+str(idx), 'OARs_DL')
+    img_file  = os.path.join(out_path, 'Native', str(idx), ddbb+'_'+str(idx)+'_VOI.nii.gz')
+    seg_file = os.path.join(out_path, 'Native', str(idx), ddbb+'_'+str(idx)+'_OARs.nii.gz')
+    save_path = os.path.join(out_path, 'Native', 'DICOM', str(idx))
+    # For the moment, we can only translate CTs and their segmentations to DICOM; not MR images
+    if modality=='CT':
+        export2dicomRT(img_file, seg_file, save_path, 'OARs_DLUS', modality)
         
 # 4. DISTANCE MAP COMPUTATION
 print("-------------------------------------------------------------------------------------------------------")
