4067559afdf70afe39a68e810b3e0e3a96a6e579
luciacubero <83298381+luciacubero@users.noreply.github.com>
2023-05-09 12:43:06 +0200

    Update load_data.py

--- utils/load_data.py
+++ utils/load_data.py
@@ -5,6 +5,8 @@ import SimpleITK as sitk
 from dcmrtstruct2nii import dcmrtstruct2nii, list_rt_structs
 import shutil
 
+from .utilities import get_directory_paths
+
 def load_data(data_path, out_path, mode:str, modality:str='CT'):
     """
     data_path : path to original data
@@ -35,7 +37,7 @@ def get_image(patient_fold, data_path, out_path):
     path_im = os.path.join(data_path, patient_fold, 'img')
     if not os.path.exists(path_im): 
         print('%s does not exist. Check path !!!' %(path_im))
-    path_segs = os.path.join(data_path, patient_fold, '_mOAR')
+    path_segs = os.path.join(data_path, patient_fold, 'mOAR')
     
     return path_im, path_nii, file_nii, path_segs
 
@@ -76,9 +78,11 @@ def dicom_to_nifti(data_path, out_path):
             path_segs = get_directory_paths(path_segs)
             path_seg = path_segs[0]
             print(list_rt_structs(path_seg))
+            final_names = [] # Names for converted structures
+            OARstructs_list = [] # List of structures to convert
             
-            try:
 
+            try:
                 # Possible names for OARs structures (add new names if necessary)
                 rectum_names   = ['rectum', 'retto', 'paroirectale', 'paroi rectale', 'rectum_ext']  # Rectum
                 rectum_names += [a+'_ct' for a in rectum_names] + [a+'_irm' for a in rectum_names] + [a+'_irm_abr' for a in rectum_names]
@@ -88,19 +92,37 @@ def dicom_to_nifti(data_path, out_path):
                 prostate_names += [a+'_ct' for a in prostate_names] + [a+'_irm' for a in prostate_names] + [a+'_irm_abr' for a in prostate_names]
                 SV_names       = ['sv' , 'seminalvesicles', 'seminal_vesicles', 'vseminales', 'vs', 'vesicules_seminales']  # Seminal Vesicles
                 SV_names += [a+'_ct' for a in SV_names] + [a+'_irm' for a in SV_names] + [a+'_irm_abr' for a in SV_names]
+                urethra_names       = ['uretre' , 'urethra']  # Urethra
+                urethra_names += [a+'_ct' for a in urethra_names] + [a+'_irm' for a in urethra_names] + [a+'_irm_abr' for a in urethra_names]
 
                 # Convert dicom-RTSTRUCT files to nifti
                 listt = list_rt_structs(path_seg)
-                rectumName   = listt[[i for i, _ in enumerate(listt) if _.casefold()==(list(set([l.casefold() for l in listt]) & set(rectum_names))[0])][0]]
-                bladderName  = listt[[i for i, _ in enumerate(listt) if _.casefold()==(list(set([l.casefold() for l in listt]) & set(bladder_names))[0])][0]]
-                prostateName = listt[[i for i, _ in enumerate(listt) if _.casefold()==(list(set([l.casefold() for l in listt]) & set(prostate_names))[0])][0]]
-                OARstructs_list = [rectumName, bladderName, prostateName] # List of structures to convert
+                try:
+                    rectumName   = listt[[i for i, _ in enumerate(listt) if _.casefold()==(list(set([l.casefold() for l in listt]) & set(rectum_names))[0])][0]]
+                    OARstructs_list.append(rectumName)
+                    final_names.append('Rectum')
+                except: pass
+                try:
+                    bladderName  = listt[[i for i, _ in enumerate(listt) if _.casefold()==(list(set([l.casefold() for l in listt]) & set(bladder_names))[0])][0]]
+                    OARstructs_list.append(bladderName)
+                    final_names.append('Bladder')
+                except: pass
+                try:
+                    prostateName = listt[[i for i, _ in enumerate(listt) if _.casefold()==(list(set([l.casefold() for l in listt]) & set(prostate_names))[0])][0]]
+                    OARstructs_list.append(prostateName)
+                    final_names.append('Prostate')
+                except: pass
                 try:
                     svName       = listt[[i for i, _ in enumerate(listt) if _.casefold()==(list(set([l.casefold() for l in listt]) & set(SV_names))[0])][0]]
                     OARstructs_list.append(svName)
-                except:
-                    print('No seminal vesicles in manual OARs segmentations')
-                print(OARstructs_list)
+                    final_names.append('SeminalVesicles')
+                except: pass
+                try:
+                    urethraName       = listt[[i for i, _ in enumerate(listt) if _.casefold()==(list(set([l.casefold() for l in listt]) & set(urethra_names))[0])][0]]
+                    OARstructs_list.append(urethraName)
+                    final_names.append('Urethra')
+                except: pass
+                print('Selected masks :            ', OARstructs_list)
 
                 # Save as NIfTI
                 path_file_nii = os.path.join(out_path, 'GTs', patient_fold)
@@ -108,25 +130,16 @@ def dicom_to_nifti(data_path, out_path):
                     os.makedirs(path_file_nii)
                 dcmrtstruct2nii(path_seg, path_im, path_file_nii, structures=OARstructs_list)
 
+                # Change masks names:
                 for i, OAR in enumerate(OARstructs_list):
-                    file_img   = path_file_nii + '/mask_'+OAR+'.nii.gz'
-                    seg   = sitk.ReadImage(file_img)
-                    segg   = sitk.GetArrayFromImage((seg==255))
-                    if i==0: labelMap_np = sitk.GetArrayFromImage(seg)
-                    labelMap_np[tuple([segg==1])]   = i+1
-
-                labelMap = sitk.GetImageFromArray(labelMap_np)
-                labelMap.CopyInformation(sitk.ReadImage(path_file_nii + '/image.nii.gz'))
-                print('Labels: ', np.unique(labelMap_np))
-                file_seg_name = path_file_nii+'/labelMap.nii.gz'
-                print('Saving... '+ file_seg_name)
-                sitk.WriteImage(labelMap, file_seg_name, True)
+                    if os.path.exists(os.path.join(path_file_nii, 'mask_'+OAR+'.nii.gz')):
+                        os.rename(os.path.join(path_file_nii, 'mask_'+OAR+'.nii.gz'), os.path.join(path_file_nii, 'mask_'+final_names[i]+'.nii.gz'))
             
             except:
                 print('\n --------------------------- ERROR WHEN LOADING THIS CASE mOAR SEGMENTATIONS --------------------------------- \n')
                 continue
-                
-        
+                                
+    
 def nifti_data(data_path, out_path):
     """
     Function to copy NIfTI image to out_path in appropriate format.
@@ -139,11 +152,8 @@ def nifti_data(data_path, out_path):
             print('Avoiding the file: ', patient_fold)
         
         print('\nProcessing case: ', patient_fold)
-        path_im, path_nii, file_nii = get_image(patient_fold, data_path, out_path)
-        #Check if image already loaded
-        if os.path.exists(file_nii):
-            print(file_nii, '     already loaded')
-            continue        
+        path_im, path_nii, file_nii, path_segs = get_image(patient_fold, data_path, out_path)
+        #Check if image already loaded    
         path_im = get_directory_paths(path_im)[0]
         print(path_im)
         image = sitk.ReadImage(path_im)
@@ -156,3 +166,64 @@ def nifti_data(data_path, out_path):
         if not os.path.exists(path_nii):
             os.makedirs(path_nii)
         shutil.copyfile(path_im, file_nii)
+        
+        # Segmentation ###############################
+        if os.path.exists(path_segs):
+            print('----- Segmentation -----')    
+            path_segs = get_directory_paths(path_segs)
+            listt = [s.split('/')[-1].removesuffix('.nii.gz').removeprefix('mask_') for s in path_segs]
+            print(listt) # They have to match the possible names. If not, the code won't work
+            final_names = [] # Names for converted structures
+            OARstructs_list = [] # List of structures to convert
+            
+            try:
+                # Possible names for OARs structures (add new names if necessary)
+                rectum_names   = ['rectum', 'retto', 'paroirectale', 'paroi rectale', 'rectum_ext']  # Rectum
+                rectum_names += [a+'_ct' for a in rectum_names] + [a+'_irm' for a in rectum_names] + [a+'_irm_abr' for a in rectum_names]
+                bladder_names  = ['bladder', 'vescica', 'vessie', 'paroivesicale', 'vesssie', 'vessie_ext']  # Bladder
+                bladder_names += [a+'_ct' for a in bladder_names]+ [a+'_irm' for a in bladder_names] + [a+'_irm_abr' for a in bladder_names]
+                prostate_names = ['prostate', 'prosate', 'proistate', 'prosatet', 'prosatte',  'prost', 'prosta']  # Prostate
+                prostate_names += [a+'_ct' for a in prostate_names] + [a+'_irm' for a in prostate_names] + [a+'_irm_abr' for a in prostate_names]
+                SV_names       = ['sv' , 'seminalvesicles', 'seminal_vesicles', 'vseminales', 'vs', 'vesicules_seminales']  # Seminal Vesicles
+                SV_names += [a+'_ct' for a in SV_names] + [a+'_irm' for a in SV_names] + [a+'_irm_abr' for a in SV_names]
+                urethra_names       = ['uretre' , 'urethra']  # Urethra
+                urethra_names += [a+'_ct' for a in urethra_names] + [a+'_irm' for a in urethra_names] + [a+'_irm_abr' for a in urethra_names]
+
+                try:
+                    rectumName   = listt[[i for i, _ in enumerate(listt) if _.casefold()==(list(set([l.casefold() for l in listt]) & set(rectum_names))[0])][0]]
+                    OARstructs_list.append([path for path in path_segs if rectumName in path][0])
+                    final_names.append('Rectum')
+                except: pass
+                try:
+                    bladderName  = listt[[i for i, _ in enumerate(listt) if _.casefold()==(list(set([l.casefold() for l in listt]) & set(bladder_names))[0])][0]]
+                    OARstructs_list.append([path for path in path_segs if bladderName in path][0])
+                    final_names.append('Bladder')
+                except: pass
+                try:
+                    prostateName = listt[[i for i, _ in enumerate(listt) if _.casefold()==(list(set([l.casefold() for l in listt]) & set(prostate_names))[0])][0]]
+                    OARstructs_list.append([path for path in path_segs if prostateName in path][0])
+                    final_names.append('Prostate')
+                except: pass
+                try:
+                    svName       = listt[[i for i, _ in enumerate(listt) if _.casefold()==(list(set([l.casefold() for l in listt]) & set(SV_names))[0])][0]]
+                    OARstructs_list.append([path for path in path_segs if svName in path][0])
+                    final_names.append('SeminalVesicles')
+                except: pass
+                try:
+                    urethraName       = listt[[i for i, _ in enumerate(listt) if _.casefold()==(list(set([l.casefold() for l in listt]) & set(urethra_names))[0])][0]]
+                    OARstructs_list.append([path for path in path_segs if urethraName in path][0])
+                    final_names.append('Urethra')
+                except: pass
+                print('Selected masks :            ', OARstructs_list)
+
+                # Save as NIfTI
+                path_file_nii = os.path.join(out_path, 'GTs', patient_fold)
+                if not os.path.exists(path_file_nii):
+                    os.makedirs(path_file_nii)
+
+                # Copy files and change names:
+                for i, OAR in enumerate(OARstructs_list): shutil.copy(OAR, os.path.join(path_file_nii, 'mask_'+final_names[i]+'.nii.gz'))
+            
+            except:
+                print('\n --------------------------- ERROR WHEN LOADING THIS CASE mOAR SEGMENTATIONS --------------------------------- \n')
+                continue
